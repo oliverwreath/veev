947dd8eff88a958099eeab7e2e91ccab688ddbf1
oliverwreath <skywalkerhunter@gmail.com>
2019-05-09 14:35:42 -0400

    cleanUp logger.

--- src/test/java/com/oliver/DocumentTest.java
+++ src/test/java/com/oliver/DocumentTest.java
@@ -5,8 +5,6 @@ import org.apache.commons.lang3.Validate;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
@@ -20,11 +18,9 @@ import java.util.Map;
  */
 @Slf4j
 public class DocumentTest {
-    private static final Logger logger = LoggerFactory.getLogger(DocumentTest.class);
-
     @BeforeAll
     static void setUp() {
-        logger.info("Test Begin!\n");
+        log.info("Test Begin!\n");
     }
 
     @Test
@@ -49,12 +45,11 @@ public class DocumentTest {
         Validate.isTrue(document2.toString().equals("Document{'Boy Andrews','Apple Sauce','SHORT_NO_Truncation',87 gb,2013-05-10,2013-05-10}"));
 
         Document document3 = new Document(documentFormatter, "Cat Andrews", "Zed", "Last_WORD TOO_Long_keep_it_whole_right? these_will_not_show_since_too_long", "924 k", "2013-05-12", "2013-05-12");
-        System.out.println(document3.toString());
+        log.debug("document3.toString() = {}", document3.toString());
         Validate.isTrue(document3.toString().equals("Document{'Cat Andrews','Zed','Last_WORD TOO_Long_keep_it_whole_right?...',924 k,2013-05-12,2013-05-12}"));
     }
-
     @AfterAll
     static void cleanUp() {
-        logger.info("Test End!\n");
+        log.info("Test End!\n");
     }
 }
